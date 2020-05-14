fa997cc8778aa85b79b5154bbce22fa0ab8cd2d1
David <sanderdj90@gmail.com>
2020-05-14 15:34:15 -0600

    begin Unit testing notes

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -1,15 +1,20 @@
 using System;
-//xUnit is not a part of .NET core, it is a Nuget package available for .Net Core
+//xunit is not a part of .NET core, it is a Nuget package available for .Net Core
 using Xunit;
 
 namespace GradeBook.Tests
 {
     public class UnitTest1
     {
+        // Fact is an ATTRIBUTE in C# 
+        // an attribute is a little piece of data that is attached to the symbol that follow it.
+        // In the example below, Fact is a little piece of data or ATTRIBUTE that is ATTACHED to Test1()
+        // We decorate unit testing methods with the Fact method
         [Fact]
         public void Test1()
         {
 
         }
+       
     }
 }
