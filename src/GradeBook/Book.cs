fbeeb15a38e86a4acb7522d0dfae34d26b2b2474
David <sanderdj90@gmail.com>
2020-05-14 15:00:23 -0600

    feat: Add GetHighestGrade method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -7,15 +7,24 @@ namespace GradeBook
     class Book
     {
         //Write an explicit constructor if a constructor is not provided .NET does the default initialization
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            // the this (keyword) refers to the object that is currently being operated on. 
+            // Assign the parameter name (above) to the Books name variable within Class books.
+            this.name = name;
         }
         // Field Definitition, looks very much like a variable declaration
+
+        // Add grade is an INSTANCE MEMBER of the class book. 
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
+        // ACCESS MODIFIERS
+        // The keyword `public` is an accesss modifier, it controls where you can use that function it precedes.
+        // public means code outside of this class can be used outside of the book class
+        // The method GetGradeAverage is a MEMBER of the Class book
         public void GetGradeAverage()
         {
             // var (implicit typing) vs using explicity typing string, int, double, byte, etc.
@@ -30,7 +39,19 @@ namespace GradeBook
             avg = sum / grades.Count;
             Console.WriteLine($"The student's average grade is {avg:N2}");
         }
-        
-        List<double> grades;
+        public void GetHighestGrade() {
+            var highestGrade = 0.0;
+            foreach(var grade in grades) {
+                if(grade > highestGrade) {
+                    highestGrade = grade;
+                }
+            }
+            Console.WriteLine(highestGrade);
+        }
+        // The private keyword make it so the variable/method is only avaiable to other members of the class.
+        // the variables below are called INSTANCE FIELDS
+        private List<double> grades;
+        private string name;
+    
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -16,16 +16,18 @@ namespace GradeBook
     class Program
     {
         // Main is a method (a function within an object.)
+        // What is the static keyword?
+        // The static keyword negates some of the benefits of object oriented programming by violating rules of encapsulation if used improperly
         static void Main(string[] args)
         {
-            var book = new Book();
-            Book book2 = new Book();
+            var book = new Book("David's Grade Book");
+            Book book2 = new Book("Morty's Grade Book");
             book.AddGrade(89.3);
             book2.AddGrade(43.4);
             book2.AddGrade(90.4);
             book2.AddGrade(79.4);
             book2.AddGrade(80.4);
-            book2.GetGradeAverage();
+            book2.GetHighestGrade();
         }
     }
 }
